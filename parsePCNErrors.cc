45df4607e082c8e0bdcde4218e845c0f6f47e19f
Suvayu Ali <Suvayu.Ali@cern.ch>
2012-03-24 23:42:58 +0100

    Cleanup and document log parser

--- parsePCNErrors.cc
+++ parsePCNErrors.cc
@@ -1,24 +1,36 @@
+/**
+ * @file   parsePCNErrors.cc
+ * @author Suvayu Ali <Suvayu.Ali@cern.ch>
+ * @date   Sat Mar 24 02:20:55 2012
+ *
+ * @brief  CLI utility to parse Vetra log files and dump ROOT trees.
+ *         Parsing equivalent to: (parse only rows with numbers or white space)
+ *         $ sed -ne '/^ *|[0-9 |]\\+| *$/ {s/|//gp}' logfile > space-separated-tempfile
+ *
+ * 	   compile as:
+ *	   $ g++ -o parsePCNErrors -Wall $(root-config --cflags --libs) parsePCNErrors.cc
+ *
+ */
+
+// STL
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
-#include <cstdio>
 #include <cstdlib>
-#include <cassert>
-#include <bitset>
 #include <exception>
-#include <typeinfo>
+#include <cassert>
 
+// may need later
+#include <bitset>
+#include <cstdio>
+
+// ROOT classes
 #include <TString.h>
-#include <TH2D.h>
-#include <TPad.h>
-#include <TCanvas.h>
 #include <TTree.h>
 #include <TFile.h>
 
+// BOOST classes
 #include <boost/foreach.hpp>
-
-// #include "utils.hh"
 
 
 template <class T> void test(std::vector<T> &col);
@@ -26,7 +38,23 @@ template <class T> void test(std::vector<T> &col);
 int main(int argc, char *argv[])
 {
   if (argc == 1 or argc % 2 != 1) {
-    std::cout << "Insufficient/incorrect number of arguments." << std::endl; 
+    std::cout << "Insufficient/incorrect number of arguments."
+	      << std::endl << std::endl;
+    std::cout << "Usage: ./parsePCNErrors [options] --file <input log file>"
+	      << std::endl << std::endl;
+    std::cout << "   --file    Log file with input data in an ascii table, e.g."
+	      << std::endl;
+    std::cout << "             | col1 | col2 |" << std::endl;
+    std::cout << "             |------+------|" << std::endl;
+    std::cout << "             |   34 | 1010 |" << std::endl << std::endl;
+    std::cout << "   --header  Table header string for TTree::ReadFile() (default below)."
+	      << std::endl;
+    std::cout << "             runNo/L:eventID/L:tell1/I:ExpPCN/I:Beetle/I:expbits/C:badbits/C"
+	      << std::endl << std::endl;
+    std::cout << "   --temp    Temporary file to use (default: /tmp/PCNErrors.txt)."
+	      << std::endl << std::endl;
+    std::cout << "   --output  ROOT file to dump TTree (default: PCNErrors.root)."
+	      << std::endl;
     return 1;
   }
 
@@ -36,7 +64,7 @@ int main(int argc, char *argv[])
   }
 
   // program options
-  std::string inFile, tmpFile, outFile, cmdstring;
+  std::string inFile, tmpFile, outFile, header, cmdstring;
 
   assert(argc % 2);
   for (unsigned int i = 0; i < arguments.size(); i+=2) {
@@ -45,11 +73,15 @@ int main(int argc, char *argv[])
     if ( opt.Contains("--file") )   inFile  = value.Data();
     if ( opt.Contains("--temp") )   tmpFile = value.Data();
     if ( opt.Contains("--output") ) outFile = value.Data();
+    if ( opt.Contains("--header") ) header  = value.Data();
   }
 
   if (tmpFile == "") tmpFile = "/tmp/PCNErrors.txt";
   if (outFile == "") outFile = "PCNErrors.root";
+  if (header  == "")
+    header = "runNo/L:eventID/L:tell1/I:ExpPCN/I:Beetle/I:expbits/C:badbits/C";
 
+  // parse only rows with numbers or white space
   cmdstring = "sed -ne '/^ *|[0-9 |]\\+| *$/ {s/|//gp}' "
     + inFile + " > " + tmpFile;
 
@@ -59,8 +91,7 @@ int main(int argc, char *argv[])
   }
 
   TTree ftree("ftree", "PCN error tree");
-  ftree.ReadFile(tmpFile.c_str(),
-		 "runNo/L:eventID/L:tell1/I:ExpPCN/I:Beetle/I:expbits/C:badbits/C");
+  ftree.ReadFile(tmpFile.c_str(), header.c_str());
 
   TFile file(outFile.c_str(), "recreate");
   ftree.Write();
